fdd5aa44209964d875028ebba0f9282af3365cd5
YeZhengXiong <382611619@qq.com>
2023-09-03 14:46:58 +0800

    first commit

---
+++ es/src/button/index.mjs
@@ -0,0 +1,7 @@
+import t from "./button.vue_vue_type_script_setup_true_lang.mjs";
+import { withInstall as o } from "@roses/utils";
+const a = o(t);
+export {
+  a as Button,
+  a as default
+};
---
+++ es/src/icon/index.mjs
@@ -0,0 +1,7 @@
+import o from "./icon.vue_vue_type_script_setup_true_lang.mjs";
+import { withInstall as t } from "@roses/utils";
+const a = t(o);
+export {
+  a as Icon,
+  a as default
+};

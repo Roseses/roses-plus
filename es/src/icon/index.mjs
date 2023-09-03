de963e5d1fb6ca613495ded249681dfe2c3117b7
YeZhengXiong <382611619@qq.com>
2023-09-03 23:08:54 +0800

    feat: 调整包引入

---
+++ es/src/icon/index.mjs
@@ -0,0 +1,7 @@
+import o from "./icon.vue_vue_type_script_setup_true_lang.mjs";
+import { withInstall as t } from "@roseslove/utils";
+const a = t(o);
+export {
+  a as Icon,
+  a as default
+};
---
+++ lib/src/button/index.d.ts
@@ -0,0 +1,28 @@
+export declare const Button: import("@roseslove/utils/dist/lib/withinstall").SFCWithInstall<import("vue").DefineComponent<{
+    type: {
+        type: StringConstructor;
+        required: false;
+    };
+    size: {
+        type: StringConstructor;
+        required: false;
+    };
+}, {
+    buttonProps: {
+        type?: string | undefined;
+        size?: string | undefined;
+    };
+    buttonStyle: import("vue").ComputedRef<{
+        [x: string]: string | undefined;
+    }>;
+}, unknown, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, Record<string, any>, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<{
+    type: {
+        type: StringConstructor;
+        required: false;
+    };
+    size: {
+        type: StringConstructor;
+        required: false;
+    };
+}>>, {}>>;
+export default Button;
